d6e67366cc203cbd97becc00bf632fcec5e674f7
Joe Hertig <joehertig94@gmail.com>
2021-08-10 13:02:00 -0600

    fight function

---
+++ assets/js/game.js
@@ -0,0 +1,16 @@
+// window prompt
+var playerName = window.prompt("What is your robot's name?");
+// what is this?
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("our robot's name is " + playerName);
+
+// this creates a funciton named "fight"
+function fight() {
+  window.alert("The fight has begun!");
+}
+
+// fight();
