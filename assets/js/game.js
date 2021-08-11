3c006c617aeb35bcedb3e52a3fde40391eeb63da
Joe Hertig <joehertig94@gmail.com>
2021-08-10 18:11:48 -0600

    array's

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,7 +7,14 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames.length - 1);
+for (var i = 0; i < 3; i++) {
+  console.log("apple");
+}
 var enemyHealth = 50;
 var enemyAttack = 12;
 
@@ -84,4 +91,4 @@ var fight = function () {
 };
 
 // run fight function to start game
-fight();
+// fight();
